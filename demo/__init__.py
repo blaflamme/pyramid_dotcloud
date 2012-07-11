83a7d166a0e1513ea4757665bb7bd78670dc2432
Blaise Laflamme <blaise@laflamme.org>
2012-07-11 16:33:37 -0400

    changed view name

--- demo/__init__.py
+++ demo/__init__.py
@@ -3,12 +3,12 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 
 
-def dotcloud_env(request):
+def dotcloud_env_view(request):
     env = request.dotcloud_env
     return Response('<h2>DOTCLOUD ENV</h2>{0}'.format(env.__dict__))
 
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-    config.add_view(dotcloud_env)
+    config.add_view(dotcloud_env_view)
     return config.make_wsgi_app()
